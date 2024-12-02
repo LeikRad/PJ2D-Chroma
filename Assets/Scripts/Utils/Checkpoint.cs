c09e62b30d82d29d74261d783a055f7d63d94a02
Nadjito <leonardofalcao1707@gmail.com>
2024-12-02 22:04:54 +0000

    Changed the respawn script to allow the same checkpoint in multiple scripts

--- Assets/Scripts/Utils/Checkpoint.cs
+++ Assets/Scripts/Utils/Checkpoint.cs
@@ -1,17 +1,25 @@
 ﻿using UnityEngine;
- 
+
 public class Checkpoint : MonoBehaviour
 {
-    public CircleCollider2D trigger;
- 
+    public BoxCollider2D trigger;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
-            RespawnController.Instance.respawnPoint = transform;
+            // get gameobject with tag respawn
+            GameObject respawn = GameObject.FindGameObjectWithTag("Respawn");
+            // for each child in respawn update the position to the current checkpoint
+            foreach (Transform child in respawn.transform)
+            {
+                RespawnController respawnController = child.GetComponent<RespawnController>();
+                respawnController.respawnPoint = transform;
+            }
             trigger.enabled = false;
         }
     }
+
     private void OnTriggerExit2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
