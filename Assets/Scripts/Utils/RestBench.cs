2ccc98f9e0248ed95e9e6cdf6bf5b866b3e8099f
RitaSimoes566 <ritasimoes0@ua.pt>
2025-01-20 13:17:27 +0000

    Checkpoint Sprites

--- Assets/Scripts/Utils/RestBench.cs
+++ Assets/Scripts/Utils/RestBench.cs
@@ -2,12 +2,13 @@
 
 public class RestBench : MonoBehaviour
 {
-    public static Transform benchRespawnPoint; 
+    public static Transform benchRespawnPoint;
+    //private Animator player_animator = Player.Instance.GetComponent<Animator>();
 
     private void Start()
     {
         benchRespawnPoint = transform;
-        RespawnManager.Instance.SetBenchRespawnPoint(benchRespawnPoint); 
+        RespawnManager.Instance.SetBenchRespawnPoint(benchRespawnPoint);
     }
 
     private void Update()
@@ -15,6 +16,11 @@ public class RestBench : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.E) && Vector2.Distance(transform.position, Player.Instance.transform.position) < 2f)
         {
             SitAndRest();
+            //player_animator.SetBool("IsBench", true);
+        }
+        else
+        {
+            //player_animator.SetBool("IsBench", false);
         }
     }
 
