5e324a10110e18e73ca439d55f24841d46e8ede8
LeikRad <48689668+LeikRad@users.noreply.github.com>
2025-01-26 23:21:00 +0000

    Paint stuff

--- Assets/Scripts/Utils/Projectile.cs
+++ Assets/Scripts/Utils/Projectile.cs
@@ -7,12 +7,16 @@ public class Projectile : MonoBehaviour
     public float maxDistance = 10f;
 
     private Vector3 startPosition;
+    
+    public GameObject splatPrefab;
+    public Transform splatHolder;
 
     private void Start()
     {
         startPosition = transform.position;
         Rigidbody2D rb = GetComponent<Rigidbody2D>();
         rb.linearVelocity = transform.right * speed; // Certifique-se de que o projétil está se movendo
+        splatHolder = GameObject.Find("SplatHolder").transform;
     }
 
     private void Update()
@@ -37,6 +41,14 @@ public class Projectile : MonoBehaviour
         else if (collision.gameObject.CompareTag("Enemy"))
         {
             collision.gameObject.GetComponent<Health>()?.TakeDamage(damage);
+            Destroy(gameObject);
+        }
+        else if (collision.gameObject.layer == LayerMask.NameToLayer("Floors"))
+        {
+            GameObject splat = Instantiate(splatPrefab, transform.position, Quaternion.identity);
+            splat.transform.SetParent(splatHolder);
+            Splat splatScript = splat.GetComponent<Splat>();
+            splatScript.Initialize(Splat.SplatLocation.Foreground);
             Destroy(gameObject);
         }
         else
